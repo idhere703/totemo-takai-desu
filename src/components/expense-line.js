21ec85b11c1202b3786202e41ea13295507ea75e
Brandon <idhere703@gmail.com>
2017-06-24 16:27:06 -0400

    Fixed issue where create was failing. Still looks terrible though.

--- src/components/expense-line.js
+++ src/components/expense-line.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 class ExpenseLine extends React.Component {
 
+
+
   render() {
     return (
       <div>
         <div>
           { this.props.expense.label }
         </div>
-        <input onChange={ this.props.expenseChange(this.props.expense.id, this.props.expense.amount) } value={ this.props.expense.amount } />
+        <input onChange={ () => {
+                            this.props.expenseChange(this.props.expense.id, this.props.expense.amount)
+                          } } value={ this.props.expense.amount } />
       </div>);
   }
 }
--- src/components/expenses.js
+++ src/components/expenses.js
@@ -8,26 +8,37 @@ class Expenses extends React.Component {
         this.state = {
             expenses: []
         };
+
+        this.addExpense = this.addExpense.bind(this);
+        this.expenseChange = this.expenseChange.bind(this);
     }
 
     addExpense() {
-        this.state.expenses.push({
+        const expenses = this.state.expenses;
+        expenses.push({
             label: '',
             amount: 0,
             category: null,
             id: this.state.expenses.length
         });
+        this.setState({
+            expenses
+        });
     }
 
     expenseChange(expenseId, value) {
-        this.state.expenses[expenseId].amount = value;
+        const expenses = this.state.expenses;
+        expenses[expenseId].amount = value;
+        this.setState({
+            expenses
+        })
     }
 
     render() {
         return (
             <div>
               { this.state.expenses.map((expense) => {
-                    return (<ExpenseLine expense={ expense } />);
+                    return (<ExpenseLine expense={ expense } expenseChange={ this.expenseChange } />);
                 }) }
               <button onClick={ this.addExpense }>Add expense</button>
             </div>);
