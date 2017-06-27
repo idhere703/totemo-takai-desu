5a9a728c23d157996f7df37425cacf694c8cdc1c
Brandon <idhere703@gmail.com>
2017-06-26 20:53:31 -0400

    Fixed broken build.

--- src/data/ExpensesStore.js
+++ src/data/ExpensesStore.js
@@ -3,6 +3,7 @@ import { ReduceStore } from 'flux/utils';
 import ExpensesActionTypes from './ExpensesActionTypes';
 import ExpensesDispatcher from './ExpensesDispatcher';
 import Counter from './Counter';
+import Expense from './Expense';
 
 class ExpensesStore extends ReduceStore {
     constructor() {
@@ -16,13 +17,12 @@ class ExpensesStore extends ReduceStore {
     reduce(state, action) {
         switch (action.type) {
             case ExpensesActionTypes.ADD_EXPENSE:
-            case TodoActionTypes.ADD_TODO:
                 // Don't add expense with no value.
                 if (!action.value) {
                     return state;
                 }
                 const id = Counter.increment();
-                return state.set(id, new Todo({
+                return state.set(id, new Expense({
                     id,
                     value: action.value,
                     category: action.category,
