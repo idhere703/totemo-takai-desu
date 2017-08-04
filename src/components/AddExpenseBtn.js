0ce9c97128a0239773048f2e1384412e00c10823
Brandon <idhere703@gmail.com>
2017-08-04 19:13:17 -0400

    Added spacebar add and focus.

--- src/components/AddExpenseBtn.js
+++ src/components/AddExpenseBtn.js
@@ -1,5 +1,24 @@
 import React from 'react';
 export default (props) => {
+    // capture keyboard input
+    document.onkeypress = (e) => {
+        // check for tab
+        if (e.keyCode == 32) {
+            // check if an input is currently in focus
+            // if (document.activeElement.nodeName.toLowerCase() != "input") {
+            // prevent default spacebar event (scrolling to bottom)
+            e.preventDefault();
+            console.log('Event detected');
+            props.onAddExpense({
+                value: 1,
+                label: `Expense-${props.expenses.size}`
+            });
+            // Shift focus to new event line item.
+            const expenseItem = document.getElementsByName("expense-line-item")[0];
+            if (expenseItem) expenseItem.focus();
+        // }
+        }
+    };
     return (
         <button type="button" onClick={ () => props.onAddExpense({
                                     value: 1,
--- src/views/ExpensesView.js
+++ src/views/ExpensesView.js
@@ -2,41 +2,38 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 const ExpensesView = (props) => {
-    return <div>
-             <Header {...props} />
-             <Main {...props} />
-             <Footer {...props} />
-           </div>;
+  return <div>
+           <Header {...props} />
+           <Main {...props} />
+           <Footer {...props} />
+         </div>;
 };
 
 export default ExpensesView;
 
 const Main = (props) => {
-    const expenses = [...props.expenses.values()].reverse();
-    if (expenses.length === 0) {
-        return null;
-    }
-    return (
-        <section>
-          <ul>
-            { expenses.map(expense => (
-                  <li key={ expense.id }>
-                    <label>
-                      { expense.label || `Expense: ${expense.id}` }
-                    </label>
-                    <div>
-                      <input type="number" onChange={ (event) => {
-                                                          const value = event.target.value;
-                                                          props.onEditExpense({
-                                                              value,
-                                                              id: expense.id
-                                                          });
-                                                      } } />
-                      <span onClick={ event => props.onDeleteExpense(expense.id) } className="fa fa-remove clickable"></span>
-                    </div>
-                  </li>
-              )) }
-          </ul>
-        </section>
-        );
+  const expenses = [...props.expenses.values()].reverse();
+  if (expenses.length === 0) {
+    return null;
+  }
+  return (
+    <section>
+      <ul>
+        { expenses.map(expense => (
+            <li key={ expense.id }>
+              <label>
+                { expense.label || `Expense: ${expense.id}` }
+              </label>
+              <div>
+                <input type="number" onChange={ (event) => props.onEditExpense({
+                                                  value: event.target.value,
+                                                  id: expense.id
+                                                }) } name="expense-line-item" />
+                <span onClick={ event => props.onDeleteExpense(expense.id) } className="fa fa-remove clickable"></span>
+              </div>
+            </li>
+          )) }
+      </ul>
+    </section>
+    );
 }
