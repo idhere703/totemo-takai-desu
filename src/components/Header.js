7e9f704c5f36a924cab3a0a7fc2e7c6ffc68ebc8
Brandon <idhere703@gmail.com>
2017-08-14 19:23:52 -0400

    Finally, header styling.

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ export default (props) => {
   return (
     <header className="App-header">
       <div>Expenses</div>
-      <div>
+      <div className="App-header--add-expense">
         <AddExpenseBtn {...props} />
       </div>
-      <div>
+      <div className="App-header--upload-btn">
         <FileUpload {...props} />
       </div>
     </header>
