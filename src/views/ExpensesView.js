a0860a0838009d483a0e380d2dfea6981e2317bd
Brandon <idhere703@gmail.com>
2017-11-03 18:58:02 -0400

    Removed line break.

--- src/views/ExpensesView.js
+++ src/views/ExpensesView.js
@@ -25,14 +25,16 @@ const Main = (props) => {
       <div>
         { expenses.map(expense => (
             <div className="row" key={ expense.id }>
-              <div className="col col-6">
+              <div className="col col-5">
                 <input className="App--input-label" name="expense-line-item-name" type="text" placeholder={ expense.label || `Expense: ${expense.id}` } />
               </div>
-              <div className="col col-6">
+              <div className="col col-5">
                 <input className="App--input-expense" type="number" step="0.01" onChange={ (event) => props.onEditExpense({
                                                                                              value: event.target.value,
                                                                                              id: expense.id
                                                                                            }) } name="expense-line-item" />
+              </div>
+              <div className="col col-2">
                 <span onClick={ event => props.onDeleteExpense(expense.id) } className="fa fa-remove clickable"></span>
               </div>
             </div>
